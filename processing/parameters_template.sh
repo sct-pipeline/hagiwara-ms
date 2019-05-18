159deb53e5b9b8af689a13187d92e267199088d2
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-05-18 11:24:33 -0400

    parameters_template.sh: Added file for custom config

---
+++ processing/parameters_template.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Environment variables for the spineGeneric study.
+
+# Set every other path relative to this path for convenience
+# Do not add "/" at the end. Path should be absolute (i.e. do not use "~")
+export PATH_PARENT="/Users/julien/spineGeneric_multiSubjects"
+
+# Path to the folder containing the BIDS dataset.
+# Do not add "/" at the end. Path should be absolute (i.e. do not use "~")
+export PATH_DATA="${PATH_PARENT}/data"
+
+# Paths to where to save the new dataset.
+# Do not add "/" at the end. Path should be absolute (i.e. do not use "~")
+export PATH_OUTPUT="${PATH_PARENT}/results"
+export PATH_QC="${PATH_PARENT}/qc"
+export PATH_LOG="${PATH_PARENT}/log"
+
+# Location of manually-corrected segmentations
+export PATH_SEGMANUAL="${PATH_PARENT}/seg_manual"
+
+# If you only want to process specific subjects, uncomment and list them here:
+# export ONLY_PROCESS_THESE_SUBJECTS=(
+#   "sub-amu01"
+#   "sub-amu02"
+# )
+
+# Number of jobs for parallel processing
+export JOBS=20
+
+# Number of jobs for ANTs routine. Set to 1 if ANTs functions crash when CPU saturates.
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
